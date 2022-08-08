949f8e2c9d5c9b59dcd6be72005e2ed276933fa6
DevDaniel13 <81439840+DevDaniel13@users.noreply.github.com>
2022-08-08 12:29:06 -0600

    Add files via upload

---
+++ src/main/java/com/example/agendafirebase/Objetos/ReferenciasFirebase.java
@@ -0,0 +1,8 @@
+package com.example.agendafirebase.Objetos;
+
+public class ReferenciasFirebase {
+    final static public String URL_DATABASE = "https://agendafirebase-e30de-default-rtdb.firebaseio.com/";
+    final static public String DATABASE_NAME = "agenda";
+    final static public String TABLE_NAME = "contactos";
+}
+
